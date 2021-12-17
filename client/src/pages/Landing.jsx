bf75d564b223df9ca37035fdc27a903bcf4cd098
Oscar Olsson <oscar.olsson94@hotmail.com>
2021-12-17 16:51:12 +0100

    grab all the trips and loop over

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
+import axios from "axios";
 
 export const Landing = () => {
     const { user } = useContext(UserContext);
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(
+                "http://localhost:3001/api/planTrip/Göteborg/Stockholm"
+            );
+            setTrips(response.data);
+        };
+        fetchData();
+    }, []);
 
     if (!user.token) return <Redirect to="/login" />;
-    return <div>LANDING PAGE</div>;
+
+    return (
+        <div>
+            {trips.map((trip, i) => (
+                <>
+                    <div>{trip.fromLocation}</div>
+                    <div>{trip.toLocation}</div>
+                </>
+            ))}
+        </div>
+    );
 };

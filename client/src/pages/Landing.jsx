738e855ebc6e4e12b1b55098d451fa7a59012345
Oscar Olsson <oscar.olsson94@hotmail.com>
2021-12-17 17:34:05 +0100

    decent rendering trips

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import SendIcon from "@mui/icons-material/Send";
 import { formatDate } from "../utils/helpers";
 
 export const Landing = () => {
@@ -20,31 +21,48 @@ export const Landing = () => {
         setTrips(response.data);
     };
 
+    const heroDivStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+    };
+
+    const paddingRight = {
+        paddingRight: 5,
+    };
+
     if (!user.token) return <Redirect to="/login" />;
 
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-                width: "100%",
-            }}
-        >
-            <div>
+        <div style={heroDivStyle}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginBottom: 10,
+                }}
+            >
                 <TextField
+                    style={paddingRight}
                     label="From"
                     variant="outlined"
                     onChange={(e) => setFromLocation(e.target.value)}
                 ></TextField>
                 <TextField
+                    style={paddingRight}
                     label="To"
                     variant="outlined"
                     onChange={(e) => setToLocation(e.target.value)}
                 ></TextField>
-                <Button variant="contained" onClick={handleSearch}>
+                <Button
+                    endIcon={<SendIcon />}
+                    variant="contained"
+                    onClick={handleSearch}
+                >
                     Sök Resa
                 </Button>
             </div>
@@ -61,6 +79,12 @@ export const Landing = () => {
                         key={i}
                         style={{
                             display: "flex",
+                            background: "#1976D2",
+                            border: "1px solid grey",
+                            borderRadius: 10,
+                            padding: 2,
+                            marginBottom: 2,
+                            color: "white",
                         }}
                     >
                         <Typography paddingRight={2}>
@@ -70,10 +94,12 @@ export const Landing = () => {
                             Till: {trip.toLocation}
                         </Typography>
                         <Typography paddingRight={2}>
-                            Avgår: {formatDate(trip.departureTimeAndDate)}{` - ${}`}
+                            Avgår: {formatDate(trip.departureTimeAndDate)}
+                            {` - ${trip.departureTimeAndDate.substr(11, 5)}`}
                         </Typography>
                         <Typography paddingRight={2}>
-                            Framme: {formatDate(trip.arrivalTimeAndDate)}{` - ${}`}
+                            Framme: {formatDate(trip.arrivalTimeAndDate)}
+                            {` - ${trip.departureTimeAndDate.substr(11, 5)}`}
                         </Typography>
                     </div>
                 ))}

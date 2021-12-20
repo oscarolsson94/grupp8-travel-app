b18000bab6843ce614f60447e8a75ed6662d6088
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2021-12-20 18:13:30 +0100

    draft for the data structure of a booked trip

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -2,12 +2,21 @@ import mongoose from "mongoose";
 
 const BookingSchema = new mongoose.Schema(
   {
-    toLocation: { type: String, required: true, unique: true },
-    fromLocation: { type: String, required: true },
     userEmail: { type: String, required: true },
-    price: { type: String, required: true },
+    fromLocation: { type: String, required: true },
     departureTimeAndDate: { type: String, required: true },
+    toLocation: { type: String, required: true },
     arrivalTimeAndDate: { type: String, required: true },
+    passengerType: {type: String, required: true },
+    price: { type: Number, required: true },
+    train: [
+      {
+      advertisedTrainNumber: { type: String },
+      trainCarNumber: { type: Number },
+      trainCarSeat: { type: Number },
+      trainCarSeatType: { type: String }
+      }
+    ],
     transfers: [
       {
         location: { type: String },

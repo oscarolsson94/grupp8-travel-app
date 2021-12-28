c46a8e1498c7399190302da417ac99224bad61bb
Oscar Olsson <oscar.olsson94@hotmail.com>
2021-12-28 18:08:13 +0100

    deploy test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 const path = require("path");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));

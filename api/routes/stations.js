08b2f3fa0e11657fa494569e2810fa0a2512417e
Johan Charlez <johancz@users.noreply.github.com>
2022-01-04 12:44:36 +0100

    Remove logging from stations endpoint

--- api/routes/stations.js
+++ api/routes/stations.js
@@ -5,32 +5,26 @@ const router = express.Router();
 
 router.get("/from/:searchTerm", async (req, res) => {
   try {
-    console.log(req.params.searchTerm);
     const stations = await TripPlan
       .find({ fromLocation: { 
         $regex: '.*' + req.params.searchTerm + '.*',
         $options: 'i' } })
       .distinct("fromLocation");
-    console.log(stations);
     res.send(stations);
   } catch (error) {
-    console.log(error);
     res.status(404).json(error);
   }
 });
 
 router.get("/to/:searchTerm", async (req, res) => {
   try {
-    console.log(req.params.searchTerm);
     const stations = await TripPlan
       .find({ toLocation: { 
         $regex: '.*' + req.params.searchTerm + '.*',
         $options: 'i' } })
       .distinct("toLocation");
-    console.log(stations);
     res.send(stations);
   } catch (error) {
-    console.log(error);
     res.status(404).json(error);
   }
 });

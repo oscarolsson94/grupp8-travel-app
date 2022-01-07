507f9af9645ba944cabd3885bb1c5e5ecc5dec8a
Johan Charlez <johancz@users.noreply.github.com>
2022-01-07 18:09:53 +0100

    Update fieldValidation.js

--- client/src/utils/fieldValidation.js
+++ client/src/utils/fieldValidation.js
@@ -1,7 +1,15 @@
-const validEmailPattern = "^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+const patterns = {
+    validEmail: "^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+};
 
-export const validateEmail = (email) => new RegExp(validEmailPattern, "u").test(email);
+const explanations = {
+    validEmail: "E-post adressen kan ej innehålla svenska tecken, måste börja med en bokstav (A-Z), måste innehålla ett snabel-a ('@'), samt tecken innan och efter snabel-a:et.",
+}
+
+export const validateEmail = (email) => new RegExp(patterns.validEmail, "u").test(email);
 
 export default {
-    validateEmail
+    patterns: patterns,
+    explanations: explanations,
+    validateEmail: validateEmail
 };

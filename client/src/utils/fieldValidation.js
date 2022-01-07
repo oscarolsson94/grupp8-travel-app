08350fbcdc5f7be6fa0d6dd47bc6fdfd4cbb458b
Johan Charlez <johancz@users.noreply.github.com>
2022-01-07 15:28:27 +0100

    Add validateEmail() util function

---
+++ client/src/utils/fieldValidation.js
@@ -0,0 +1,7 @@
+const validEmailPattern = "^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+
+export const validateEmail = (email) => new RegExp(validEmailPattern, "u").test(email);
+
+export default {
+    validateEmail
+};

175b4f92c13221c0aab514dd0426692d88ca1bb9
Johan Charlez <johancz@users.noreply.github.com>
2022-01-11 13:48:17 +0100

    Update fieldValidation.js util module
    * Add pattern for validating name (any character at least 1 char long). This pattern is not optimal since it allows names to start and end on any character, and it allows characters not used in names (e.g. digits). A suggested pattern is included in the code, but not used since I am unclear on browser support for the Regex features used.
    * Updated util function for validating email, the regex now is now case insensitive.

--- client/src/utils/fieldValidation.js
+++ client/src/utils/fieldValidation.js
@@ -1,22 +1,26 @@
 const patterns = {
+    validName: "^.+", // simplified, something like '^[\p{L}'][ \p{L}'-]*[\p{L}]$' should probably be used, but unclear on browser-support for '\p{L}'.
     validEmail: "^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
     validPassword: "(?=.*\w)(?=.*\w)(?=.*\d)(?=.*[@$!%*#?&_.,]).{8,}",
 };
 
 const rules = {
+    validName: "Minst en bokstav",
     validEmail: "E-post adressen kan ej innehålla svenska tecken, måste börja med en bokstav (A-Z), måste innehålla ett snabel-a ('@'), samt tecken innan och efter snabel-a:et.",
     validPassword: "Minst 8 tecken, minst en liten bokstav, minst en stor bokstav, minst en siffra, samt minst en av följande tecken '@$!%*#?&_-'",
 };
 
 const messagesInvalid = {
-    firstName: `Det angivna namnet är ej giltigt.`,
-    lastName: `Det angivna namnet är ej giltigt.`,
+    name: `Det angivna namnet är ej giltigt.`,
     email: `Den angivna e-post adressen är ej giltigt. ${rules.validEmail ?? ""}`,
     password: `Det angivna namnet är ej giltigt. ${rules.validPassword ?? ""}`,
     confirmPassword: `De angivna lösenorden matchar ej.`,
 };
 
-export const validateEmail = (email) => new RegExp(patterns.validEmail, "u").test(email);
+export const validateName = (name) => new RegExp(patterns.validName, "ui").test(name);
+export const validateEmail = (email) => new RegExp(patterns.validEmail, "ui").test(email);
+export const validatePassword = (password) => new RegExp(patterns.validPassword, "u").test(password);
+export const validateConfirmPassword = (confirmPassword) => new RegExp(patterns.validConfirmPassword, "u").test(confirmPassword);
 
 export default {
     patterns: patterns,

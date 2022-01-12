2b3f99128384af4382cc36295602b75b101f8f63
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2022-01-12 10:28:33 +0100

    Adds more variables to receipt in orderConfirmation

--- client/src/pages/OrderConfirmation.jsx
+++ client/src/pages/OrderConfirmation.jsx
@@ -43,7 +43,7 @@ export const OrderConfirmation = () => {
       <Typography variant="h2" color="white">
         Orderbekräftelse
       </Typography>
-      {orderDetails && (<div className="containerStyle">
+      {orderDetails && (<div className="containerStyle" style={{ maxHeight: "100vh" }}>
         <Stepper sx={{ width: "100%" }} activeStep={2} alternativeLabel>
           <Step>
             <StepLabel>Sök resa</StepLabel>
@@ -55,46 +55,54 @@ export const OrderConfirmation = () => {
             <StepLabel>Orderbekräftelse</StepLabel>
           </Step>
         </Stepper>
+        <br />
+        <div>
+          <br />
+          <span>Tack för din bokning {user.firstName}!</span>
+        </div>
+        <br />
         <Divider flexItem textAlign="left">
           Din bokning
         </Divider>
         <div className="flexSpaceEven">
-          <div className="nameVariableColumns">
+          <div className="nameVariableColumns" >
             <Typography variant="subtitle1" padding={1}>
-              Från:
-            </Typography>
-            <Divider orientation="vertical" flexItem />
-            <Typography variant="subtitle1" padding={1}>
-              {orderDetails.fromLocation}
+              Från: {orderDetails.fromLocation}
             </Typography>
             <Divider flexItem />
             <Typography variant="subtitle1" padding={1}>
-              Avgår:
-            </Typography>
-            <Divider orientation="vertical" flexItem />
-            <Typography variant="subtitle1" padding={1}>
-              {formatDate(orderDetails.departureTimeAndDate)} -
+              Avgår: {formatDate(orderDetails.departureTimeAndDate)} -
               {formatTime(orderDetails.departureTimeAndDate)}
             </Typography>
             <Divider flexItem />
             <Typography variant="subtitle1" padding={1}>
-              Till:
-            </Typography>
-            <Divider orientation="vertical" flexItem />
-            <Typography variant="subtitle1" padding={1}>
-              {orderDetails.toLocation}
+              Till: {orderDetails.toLocation}
             </Typography>
             <Divider flexItem />
             <Typography variant="subtitle1" padding={1}>
-              Framme:
-            </Typography>
-            <Divider orientation="vertical" flexItem />
-            <Typography variant="subtitle1" padding={1}>
-              {formatDate(orderDetails.arrivalTimeAndDate)} -
+              Framme: {formatDate(orderDetails.arrivalTimeAndDate)} - 
               {formatTime(orderDetails.arrivalTimeAndDate)}
             </Typography>
+            <Divider flexItem />
+            <Typography variant="subtitle1" padding={1}>
+              Resenär: {orderDetails.passengerType} / {orderDetails.ticketClass}
+            </Typography>
+            <Divider flexItem />
+            <Typography variant="subtitle1" padding={1}>
+              Pris: {orderDetails.price} sek
+            </Typography>
+            <Divider flexItem />            
+            <Typography variant="subtitle1" padding={1}>
+              Bokningsnummer: {orderDetails._id}
+            </Typography>
+            <Divider flexItem /> 
           </div>
         </div>
+        <div>
+          <br />
+          <span>Din bokning har skickats till {user.email}. Där finns information om resan och hur du betalar din biljett.</span>
+          <br />
+        </div>        
       </div>
       )}
     </div>
